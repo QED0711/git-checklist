00b06ef43d86a7fd4d30846c9063333da18b8633
Quinn Dizon <qdizon@qdizon.fios-router.home>
2019-04-11 15:41:26 -0400

    style project selection

--- js/setProjectBanner.js
+++ js/setProjectBanner.js
@@ -2,12 +2,10 @@
 import { getCurrentProjectName, getCurrentProjectPath } from './helpers/getters';
 
 const setProjectBanner = () => {
-    let name = getCurrentProjectName();
     let path = getCurrentProjectPath();
 
     let banner = document.getElementById("project-banner");
-    banner.children[0].innerText = name
-    banner.children[1].innerText = path
+    banner.children[0].innerText = path
 }
 
 export default setProjectBanner
