20f2362684c98888d6733d2f604109ebabdda403
Quinn Dizon <qdizon@gmail.com>
2019-05-31 19:43:57 -0400

    extracted addNewItems to its own function

--- js/handleAddChecklistItem.js
+++ js/handleAddChecklistItem.js
@@ -1,19 +1,29 @@
 import fs from 'fs';
 import { getCurrentProjectPath } from './helpers/getters';
 
+const addChecklistItem = async (newItem) => {
+    // get path to current project .gitchecklist file
+    const path = getCurrentProjectPath() + "/.gitchecklist";
+    // read contents of file
+    let items = await fs.readFileSync(path, {encoding: 'utf-8'})        
+    items += `\n[] ${newItem}`
+    fs.writeFileSync(path, items)
+}
+
 const handleAddChecklistItem = () => {
 
     const addItemForm = document.getElementById('add-checklist-item');
-    addItemForm.onsubmit = async (e) => {
+    addItemForm.onsubmit = (e) => {
         // e.preventDefault()
         const newItem = [...e.target.children][1].value
-        // get path to current project .gitchecklist file
-        const path = getCurrentProjectPath() + "/.gitchecklist"
-        // read contents of file
-        let items = await fs.readFileSync(path, {encoding: 'utf-8'})        
-        items += `\n[] ${newItem}`
-        console.log(items)
-        fs.writeFileSync(path, items)
+        // // get path to current project .gitchecklist file
+        // const path = getCurrentProjectPath() + "/.gitchecklist"
+        // // read contents of file
+        // let items = await fs.readFileSync(path, {encoding: 'utf-8'})        
+        // items += `\n[] ${newItem}`
+        // console.log(items)
+        // fs.writeFileSync(path, items)
+        addChecklistItem(newItem)
     }
 
 }
